ce6f792fcf4be75038d383b1ce248ca1e113d36f
Cenk Bahçeliler <cnktbn@gmail.com>
2019-06-27 20:21:26 +0300

    Satranç Tahtası
    Bilge Adam Eğitiminde geliştirdiğim Satranç tahtası uygulamam.

---
+++ CenkBahçeliler/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace CenkBahçeliler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnTasEkle = new System.Windows.Forms.Button();
+            this.btnHareketEttir = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnTasEkle
+            // 
+            this.btnTasEkle.Location = new System.Drawing.Point(693, 92);
+            this.btnTasEkle.Name = "btnTasEkle";
+            this.btnTasEkle.Size = new System.Drawing.Size(132, 38);
+            this.btnTasEkle.TabIndex = 1;
+            this.btnTasEkle.Text = "Taş Ekle";
+            this.btnTasEkle.UseVisualStyleBackColor = true;
+            this.btnTasEkle.Click += new System.EventHandler(this.btnTasEkle_Click);
+            // 
+            // btnHareketEttir
+            // 
+            this.btnHareketEttir.Location = new System.Drawing.Point(693, 136);
+            this.btnHareketEttir.Name = "btnHareketEttir";
+            this.btnHareketEttir.Size = new System.Drawing.Size(132, 38);
+            this.btnHareketEttir.TabIndex = 2;
+            this.btnHareketEttir.Text = "Taşları Hareket Ettir";
+            this.btnHareketEttir.UseVisualStyleBackColor = true;
+            this.btnHareketEttir.Click += new System.EventHandler(this.btnHareketEttir_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(625, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "Kale.png");
+            this.ımageList1.Images.SetKeyName(1, "Fil.png");
+            this.ımageList1.Images.SetKeyName(2, "At.png");
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(693, 191);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(195, 225);
+            this.listBox1.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 514);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnHareketEttir);
+            this.Controls.Add(this.btnTasEkle);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTasEkle;
+        private System.Windows.Forms.Button btnHareketEttir;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}
+
---
+++ CenkBahçeliler/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CenkBahçeliler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            string[] Taslar = { "Fil", "Kale", "At" };
+            comboBox1.Items.AddRange(Taslar);
+            TahtaOlustur();
+        }
+        List<Label> labelListesi = new List<Label>();
+        List<PictureBox> ResimKutuListesi = new List<PictureBox>();
+        bool[] DogruMu = new bool[64];
+        int sayac = 0;
+        void TahtaOlustur()
+        {
+            int SiyahSutun = 100; int SiyahSatir = 50;
+            int BeyazSutun = 150; int BeyazSatir = 50;
+            int a = 1; int b = 0;
+
+            for (int j = 0; j < 8; j++)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    SiyahLabelOlustur(SiyahSutun, SiyahSatir);
+                    SiyahSutun += 100;
+                    BeyazLabelOlustur(BeyazSutun, BeyazSatir);
+                    BeyazSutun += 100;
+                }
+                a++;
+                SiyahSatir += 50; BeyazSatir += 50;
+                if (a % 2 == 0)
+                {
+                    SiyahSutun = 100;
+                    BeyazSutun = 150;
+                    b = SiyahSutun;
+                    SiyahSutun = BeyazSutun;
+                    BeyazSutun = b;
+                }
+                else
+                {
+                    SiyahSutun = 100;
+                    BeyazSutun = 150;
+                }
+            }
+        }
+        public void KaleResimKutusuOlustur()
+        {
+        tekrar:
+            int sayi = rnd.Next(0, 63);
+            if (DogruMu[sayi] == false)
+            {
+                PictureBox pb = new PictureBox();
+                pb.Width = 50; pb.Height = 50;
+                pb.Image = ımageList1.Images[0];
+                Point konum = new Point(labelListesi[sayi].Left, labelListesi[sayi].Top);
+                pb.Location = konum;
+                DogruMu[sayi] = true;
+                ResimKutuListesi.Add(pb);
+                this.Controls.Add(pb);
+                pb.BringToFront();
+                sayac++;
+            }
+            else
+            {
+                goto tekrar;
+            }
+        }
+        public void AtResimKutusuOlustur()
+        {
+            {
+            tekrar:
+                int sayi = rnd.Next(0, 63);
+                if (DogruMu[sayi] == false)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.Width = 50; pb.Height = 50;
+                    pb.Image = ımageList1.Images[2];
+                    Point konum = new Point(labelListesi[sayi].Left, labelListesi[sayi].Top);
+                    pb.Location = konum;
+                    DogruMu[sayi] = true;
+                    ResimKutuListesi.Add(pb);
+                    this.Controls.Add(pb);
+                    pb.BringToFront();
+                    sayac++;
+                }
+                else
+                {
+                    goto tekrar;
+                }
+            }
+        }
+        public void FilResimKutusuOlustur()
+        {
+            {
+            tekrar:
+                int sayi = rnd.Next(0, 63);
+                if (DogruMu[sayi] == false)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.Width = 50; pb.Height = 50;
+                    pb.Image = ımageList1.Images[1];
+                    Point konum = new Point(labelListesi[sayi].Left, labelListesi[sayi].Top);
+                    pb.Location = konum;
+                    DogruMu[sayi] = true;
+                    ResimKutuListesi.Add(pb);
+                    this.Controls.Add(pb);
+                    pb.BringToFront();
+                    sayac++;
+                }
+                else
+                {
+                    goto tekrar;
+                }
+            }
+        }
+        public void ListeyiYenile()
+        {
+            listBox1.Items.Clear();
+            foreach (PictureBox item in ResimKutuListesi)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+        }
+        public void SiyahLabelOlustur(int sayi1, int sayi2)
+        {
+            Label lbl = new Label();
+            Size boyut = new Size(50, 50);
+            Point konum = new Point(sayi1, sayi2);
+            lbl.Location = konum;
+            lbl.Size = boyut;
+            lbl.BackColor = Color.Black;
+            labelListesi.Add(lbl);
+            this.Controls.Add(lbl);
+        }
+        public void BeyazLabelOlustur(int sayi1, int sayi2)
+        {
+            Label lbl = new Label();
+            Size boyut = new Size(50, 50);
+            Point konum = new Point(sayi1, sayi2);
+            lbl.Location = konum;
+            lbl.Size = boyut;
+            lbl.BackColor = Color.White;
+            labelListesi.Add(lbl);
+            this.Controls.Add(lbl);
+        }
+        private void btnTasEkle_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Kale":
+                    KaleResimKutusuOlustur();
+                    ListeyiYenile();
+                    break;
+                case "Fil":
+                    FilResimKutusuOlustur();
+                    ListeyiYenile();
+                    break;
+                case "At":
+                    AtResimKutusuOlustur();
+                    ListeyiYenile();
+                    break;
+                default:
+                    MessageBox.Show("Lütfen bir taş seçiniz.");
+                    break;
+            }
+        }
+        private void btnHareketEttir_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+        Random rnd = new Random();
+        public int RandomSayiUret()
+        {
+            int rasgele = rnd.Next(0, 63);
+            return rasgele;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox item in ResimKutuListesi)
+            {
+                if (item.Image == ımageList1.Images[0])
+                {
+                    item.Left = labelListesi[RandomSayiUret()].Left;
+                    item.Top = labelListesi[RandomSayiUret()].Top;
+                }
+                else if (item.Image == ımageList1.Images[1])
+                {
+                    item.Left = labelListesi[RandomSayiUret()].Left;
+                    item.Top = labelListesi[RandomSayiUret()].Top;
+                }
+                else if (item.Image == ımageList1.Images[2])
+                {
+                    item.Left = labelListesi[RandomSayiUret()].Left;
+                    item.Top = labelListesi[RandomSayiUret()].Top;
+                }
+                else
+                {
+                    item.Left = labelListesi[RandomSayiUret()].Left;
+                    item.Top = labelListesi[RandomSayiUret()].Top;
+                }
+            }
+        }
+    }
+}
+
+
+
+
